680cae104d9cc9e0aa5513d8dd0e6a0acf9fd70d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-01 01:30:46 +0300

    Up

--- projectpack001/app.py
+++ projectpack001/app.py
@@ -7,10 +7,17 @@ app = Flask(__name__)
 def index():
   return render_template('index.html', title='Home', current_year=datetime.now().year)
 
+
 @app.route('/about')
 def about():
   return render_template('about.html', title='About', current_year=datetime.now().year)
+@app.route('/java')
+def java():
+  return render_template('java.html', title='Java', current_year=datetime.now().year)
 
+@app.route('/python')
+def python():
+  return render_template('python.html', title='Python', current_year=datetime.now().year)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
