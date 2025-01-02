85f174aee702b8cf4669ea5dafcf620869fcc915
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-02 14:41:51 +0300

    Up

---
+++ projectpack002/backend/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template, url_for
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
