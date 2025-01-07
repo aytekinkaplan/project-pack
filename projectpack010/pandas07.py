2de29268bcf75929907b80f21a43ea254cab827e
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-07 17:22:02 +0300

    Up

--- projectpack010/pandas07.py
+++ projectpack010/pandas07.py
@@ -1,24 +1,11 @@
 import pandas as pd
 
-from pandas04 import countries
-
-data = {
-    "name": ["kevin", "jane", "bob", "alice"],
-    "age": [24, 23, 25, 21],
-    "gender": ["male", "female", "male", "female"]
-}
-
-my_data = pd.DataFrame(data)
-print(my_data)
-
-print(my_data["name"])
-print(my_data["age"])
-print(my_data["gender"])
-
 countries_with_their_capitals = {
-    "Counties": ["Turkey", "Germany", "France", "Italy", "Spain"],
+    "Countries": ["Turkey", "Germany", "France", "Italy", "Spain"],
     "Capitals": ["Ankara", "Berlin", "Paris", "Rome", "Madrid"]
 }
 
 my_countries = pd.DataFrame(countries_with_their_capitals)
 print(my_countries)
+
+print(my_countries.loc[0])
