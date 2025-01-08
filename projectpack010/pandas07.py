89ce0778a32554971fd77b51af480a68abbc00ee
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-09 00:14:27 +0300

    Up

--- projectpack010/pandas07.py
+++ projectpack010/pandas07.py
@@ -9,3 +9,15 @@ my_countries = pd.DataFrame(countries_with_their_capitals)
 print(my_countries)
 
 print(my_countries.loc[0])
+
+
+data = {
+    "name": ["kevin", "jane", "bob", "alice"],
+    "age": [24, 23, 25, 21],
+    "gender": ["male", "female", "male", "female"]
+}
+
+my_data = pd.DataFrame(data)
+print(my_data)
+
+print(my_data.loc[0])
