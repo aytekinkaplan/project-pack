94eabe3018530e1da7a7878863a755b157b93a7d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-13 22:11:55 +0300

    Up

---
+++ projectpack013/project03/src/Person.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+const Person = ({ name, lastname, job, age, salary }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{lastname}</h2>
+      <h3>{job}</h3>
+      <h3>{age}</h3>
+      <h3>{salary}</h3>
+    </div>
+  );
+};
+
+Person.propTypes = {
+  name: PropTypes.string,
+  lastname: PropTypes.string,
+  job: PropTypes.string,
+  age: PropTypes.number,
+  salary: PropTypes.number,
+};
