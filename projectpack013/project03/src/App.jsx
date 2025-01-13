37c3f231edc22b76182b3ca0ab68362d3bec2538
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-13 22:23:21 +0300

    Up

--- projectpack013/project03/src/App.jsx
+++ projectpack013/project03/src/App.jsx
@@ -2,15 +2,65 @@ import "./App.css";
 import Person from "./Person";
 
 function App() {
+  const people = [
+    { name: "John", lastname: "Doe", job: "Engineer", age: 28, salary: 5000 },
+    {
+      name: "Jane",
+      lastname: "Foster",
+      job: "Designer",
+      age: 25,
+      salary: 4000,
+    },
+    { name: "Bob", lastname: "Smith", job: "Developer", age: 32, salary: 6000 },
+    {
+      name: "Alice",
+      lastname: "Johnson",
+      job: "Architect",
+      age: 30,
+      salary: 7000,
+    },
+    {
+      name: "Charlie",
+      lastname: "Brown",
+      job: "Manager",
+      age: 35,
+      salary: 5500,
+    },
+    {
+      name: "Emily",
+      lastname: "Davis",
+      job: "Salesperson",
+      age: 27,
+      salary: 4800,
+    },
+    {
+      name: "Frank",
+      lastname: "Miller",
+      job: "Accountant",
+      age: 29,
+      salary: 5200,
+    },
+    { name: "Grace", lastname: "Wilson", job: "HR", age: 31, salary: 4900 },
+    {
+      name: "Henry",
+      lastname: "Taylor",
+      job: "Marketing",
+      age: 28,
+      salary: 4700,
+    },
+  ];
   return (
     <>
-      <Person
-        name="John"
-        lastname="Doe"
-        job="Engineer"
-        age={28}
-        salary={5000}
-      />
+      {people.map((person, index) => (
+        <Person
+          key={index}
+          name={person.name}
+          lastname={person.lastname}
+          job={person.job}
+          age={person.age}
+          salary={person.salary}
+        />
+      ))}
     </>
   );
 }
