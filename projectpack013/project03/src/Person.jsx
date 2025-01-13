4fc11c738101f6a7d6c1e60cc333b04813fb2552
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-13 22:30:34 +0300

    Up

--- projectpack013/project03/src/Person.jsx
+++ projectpack013/project03/src/Person.jsx
@@ -1,8 +1,20 @@
 import PropTypes from "prop-types";
 
 const Person = ({ name, lastname, job, age, salary }) => {
+  const style = {
+    color: "red",
+    fontSize: "20px",
+    backgroundColor: "yellow",
+    border: "1px solid black",
+    margin: "10px",
+    padding: "10px",
+    borderRadius: "10px",
+    boxShadow: "10px 10px 5px 0px rgba(0,0,0,0.75)",
+    WebkitBoxShadow: "10px 10px 5px 0px rgba(0,0,0,0.75)",
+    MozBoxShadow: "10px 10px 5px 0px rgba(0,0,0,0.75)",
+  };
   return (
-    <div>
+    <div style={style}>
       <h1>{name}</h1>
       <h2>{lastname}</h2>
       <h3>{job}</h3>
