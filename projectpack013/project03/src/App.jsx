e758bdbee895f984b44dfaee51ecfac1f5385456
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-13 22:18:34 +0300

    Up

--- projectpack013/project03/src/App.jsx
+++ projectpack013/project03/src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Person from "./Person";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Person
+        name="John"
+        lastname="Doe"
+        job="Engineer"
+        age={28}
+        salary={5000}
+      />
+    </>
+  );
 }
 
 export default App;
