ba3aa24752999c94bc2498b1da8ef37cc7f2b313
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-18 07:43:12 +0300

    Up

--- projectpack013/project05/src/App.jsx
+++ projectpack013/project05/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Mermaid from "./components/Mermaid";
 
 const App = () => {
   return (
     <div>
       <Header title="Welcome to My App!" />
+      <Mermaid />
     </div>
   );
 };
