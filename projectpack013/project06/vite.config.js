a768d62488e3605aeb086fd41ca8b55903bb3e53
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-23 08:47:45 +0300

    Up

--- projectpack013/project06/vite.config.js
+++ projectpack013/project06/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createMarkdownPlugin } from "vite-plugin-md";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    createMarkdownPlugin(), // Markdown dosyalarını işler
+  ],
+  assetsInclude: ["**/*.md"],
+});
