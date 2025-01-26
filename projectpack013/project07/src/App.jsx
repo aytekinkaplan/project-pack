bdb61601799a5f508bfb42946c7dfd4f53ec14db
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2025-01-26 23:33:08 +0300

    Up

--- projectpack013/project07/src/App.jsx
+++ projectpack013/project07/src/App.jsx
@@ -1,5 +1,12 @@
+import MarkdownRenderer from "./components/MarkdownRenderer";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1>Markdown Dosyası Görüntüleyici</h1>
+      <MarkdownRenderer />
+    </div>
+  );
 }
 
 export default App;
